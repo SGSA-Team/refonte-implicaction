f4c24cb628fb7ae654e5e95917be83882b8fd48e
sofianeTiouassiouine <55562137+sofianeTiouassiouine@users.noreply.github.com>
2023-01-16 12:28:37 +0100

    feature/SGSA-97-Lien-vers-le-profil-des-auteurs-des-postes-lors-du-clique-sur-image-ou-le-nom-d'utilisateur (#26)
    * Lien-vers-le-profil-des-auteurs-des-postes-lors-du-clique-sur-image-ou-le-nom-d'utilisateur
    
    * [SGSA-97] petit refacto
    
    * [SGSA-97] petit refacto
    
    Co-authored-by: Sofiane <sofiane123459@gmail.com>
    Co-authored-by: Amine DJENNADI <amine.djennadi@ocapiat.fr>

--- frontend-implicaction/src/app/forums/components/post-card/post-card.component.ts
+++ frontend-implicaction/src/app/forums/components/post-card/post-card.component.ts
@@ -1,16 +1,18 @@
-import {Component, Input} from '@angular/core';
-import {Post} from '../../model/post';
-import {Router} from "@angular/router";
-
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../../model/post';
+import { Univers } from '../../../shared/enums/univers';
 
 @Component({
   selector: 'app-post-card',
   templateUrl: './post-card.component.html',
   styleUrls: ['./post-card.component.scss'],
 })
-export class PostCardComponent {
-  @Input()
-  post: Post;
+export class PostCardComponent implements OnInit {
+  @Input() post: Post;
+  readonly univers = Univers;
+  userProfileUrl: string;
 
+  ngOnInit(): void {
+    this.userProfileUrl = `/${this.univers.USERS.url}/${this.post.userId}/profile`;
+  }
 }
