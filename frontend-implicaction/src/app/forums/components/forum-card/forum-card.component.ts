0727ad6c2ad89207705043415cb9d7caac3984c1
sofianeTiouassiouine <55562137+sofianeTiouassiouine@users.noreply.github.com>
2023-01-16 11:36:05 +0100

    feature/SGSA-101-Lien-vers-le-forum-lors-du-clique-sur-une-ligne-du-tableau-Liste-des-forums (#25)
    * Lien vers le forum lors du clique sur une ligne du tableau Liste des forums
    
    * Update forum-card.component.html
    
    * [SGSA-101] ajout univers
    
    Co-authored-by: Sofiane <sofiane123459@gmail.com>
    Co-authored-by: Amine-maker <59418638+Amine-maker@users.noreply.github.com>
    Co-authored-by: Amine DJENNADI <amine.djennadi@ocapiat.fr>

--- frontend-implicaction/src/app/forums/components/forum-card/forum-card.component.ts
+++ frontend-implicaction/src/app/forums/components/forum-card/forum-card.component.ts
@@ -1,12 +1,19 @@
-import {Component, Input} from '@angular/core';
-import {Group} from '../../model/group';
+import { Component, Input, OnInit } from '@angular/core';
+import { Univers } from 'src/app/shared/enums/univers';
+import { Group } from '../../model/group';
 
 @Component({
   selector: 'app-forum-card',
   templateUrl: './forum-card.component.html',
   styleUrls: ['./forum-card.component.scss'],
 })
-export class ForumCardComponent {
-  @Input()
-  group: Group;
+export class ForumCardComponent implements OnInit {
+  @Input() group: Group;
+  redirectionUrl: string;
+
+  constructor() {}
+  ngOnInit(): void {
+    this.redirectionUrl = `/${Univers.FORUMS.url}/${this.group.id}`;
+    console.log(this.redirectionUrl);
+  }
 }
